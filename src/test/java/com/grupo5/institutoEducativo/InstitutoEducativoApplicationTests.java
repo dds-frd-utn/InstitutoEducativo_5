881a7b7dc79d828f73811cab9ac1a6aab4058043
GINOXCVIII <nunezolmos.ig@gmail.com>
2022-09-29 22:52:55 -0300

    first commit

---
+++ src/test/java/com/grupo5/institutoEducativo/InstitutoEducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.institutoEducativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoEducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
