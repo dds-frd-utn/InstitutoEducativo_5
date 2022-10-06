a07ccdb8f7de2abe7ccc9ba87aac49d6bd449ccd
Franco Abuin <49248726+abuinfranco@users.noreply.github.com>
2022-10-06 19:57:52 -0300

    cambios en categoriaController

--- src/main/java/com/grupo5/institutoEducativo/controller/CategoriaController.java
+++ src/main/java/com/grupo5/institutoEducativo/controller/CategoriaController.java
@@ -7,7 +7,16 @@ package com.grupo5.institutoEducativo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grupo5.institutoEducativo.entity.Categoria;
+import com.grupo5.institutoEducativo.service.CategoriaService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -16,5 +25,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/url")
 public class CategoriaController {
-    
+	@Autowired(required = false)
+    CategoriaService categoriaService;
+
+    @RequestMapping(value = "/categorias", method = RequestMethod.GET, produces = "application/json")
+    public List<Categoria> getDocentes() {
+        return categoriaService.findAllCategoria();
+    }
+
+    @RequestMapping(value = "/categorias/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Optional<Categoria> getAlumno(@PathVariable Long id) {
+        return categoriaService.findCategoriaById(id);
+    }
+
+    @RequestMapping(value = "/categorias/add", method = RequestMethod.POST, produces = "application/json")
+    public Categoria addPostPost(Categoria categoria) {
+        return categoriaService.saveCategoria(categoria);
+    }
+
+    @RequestMapping(value = "/categorias/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public String deletePost(@PathVariable Long id) {
+        return categoriaService.deleteCategoria(id);
+    }
+
+    @RequestMapping(value = "/categorias/update", method = RequestMethod.PUT, produces = "application/json")
+    public String updateCategoria(Categoria categoria) {
+        return categoriaService.updateCategoria(categoria);
+    }
+
 }
